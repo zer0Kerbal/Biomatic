c2adb710494491965ae9a536a2db31b360787f75
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2020-02-08 02:43:17 -0600

    Update Version.cs

--- Source/Properties/Version.cs
+++ Source/Properties/Version.cs
@@ -6,10 +6,10 @@ namespace Biomatic
     public static class Version
     {
         public const int major = 1;
-        public const int minor = 4;
-        public const int patch = 0;
+        public const int minor = 3;
+        public const int patch = 2;
         public const int build = 0;
-        public const string Number = "1.4.0.0";
+        public const string Number = "1.3.2.0";
 #if DEBUG
         public const string Text = Number + " BETA DEBUG";
 #else
