268264d6051aac2246dfab45096a42ea738a96ce
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2020-04-13 21:21:09 -0500

    1.3.3.7

--- Source/Biomatic.cs
+++ Source/Biomatic.cs
@@ -1065,6 +1065,7 @@ namespace Biomatic
             if (info == string.Empty)
             {
                 info += Localizer.Format("#Biomatic_manu"); // #Biomatic_manu = Biff Industries, Inc.
+                info += "\n v" + Version.Text; // Biomatic Version Number text
                 info += "\n<color=#b4d455FF>" + Localizer.Format("#Biomatic_desc"); // #Biomatic_desc = In-flight biome identifier
                 info += "\n\n<color=orange>Requires:</color><color=#FFFFFFFF> \n- <b>" + Localizer.Format("#autoLOC_252004"); // #autoLOC_252004 = ElectricCharge
                 info += "</b>: </color><color=#99FF00FF>" + RateString(ECresourceConsumptionRate) + "</color>"; 
